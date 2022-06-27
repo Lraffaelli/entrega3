e7dddcd7e051aaa117d48a039daa912e3de66384
Lraffaelli <lucasraffaelli@outlook.com>
2022-06-27 20:59:00 -0300

    correccion metodo random

--- contenedor.js
+++ contenedor.js
@@ -3,27 +3,30 @@ const fs = require("fs");
 class Contenedor {
   constructor(nameFile) {
     this.nameFile = nameFile;
-   
   }
   async getAll() {
     try {
       const contenido = await fs.promises.readFile(this.nameFile, "utf-8");
-      let productos= JSON.parse(contenido)
-      console.log(productos)
-      return productos
-      ;
-    } catch ( error){
-      console.log( error);
+      let productos = JSON.parse(contenido);
+      console.log(productos);
+      return productos;
+    } catch (error) {
+      console.log(error);
     }
   }
   async getRandom() {
-    const contenido = await fs.promises.readFile(this.nameFile, "utf-8");
-    let productos= JSON.parse(contenido)
-    let id= ()=>{
-      let result= Math.floor(Math.random() * productos.length);
-      return result}
-    console.log(productos[id()])
-    return productos[id()]
+    try {
+      const contenido = await fs.promises.readFile(this.nameFile, "utf-8");
+      let productos = JSON.parse(contenido);
+      let id = () => {
+        let result = Math.floor(Math.random() * productos.length);
+        return result;
+      };
+      console.log(productos[id()]);
+      return productos[id()];
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 module.exports = { Contenedor };
